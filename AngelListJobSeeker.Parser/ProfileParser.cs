0249ba10fe0dc324e7d553382f674ada3f58b5f6
Danilo Caballero <fabrizio.dfc2@gmail.com>
2014-12-25 21:41:44 -0800

    Initial commit

---
+++ AngelListJobSeeker.Parser/ProfileParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace AngelListJobSeeker.Parser
+{
+    public class ProfileParser
+    {
+        public static dynamic ParseFile(string fileName)
+        {
+            var fileContent = GetFileContent(fileName);
+            dynamic deserializedContent;
+
+            try
+            {
+                deserializedContent = JsonConvert.DeserializeObject(fileContent);
+            }
+            catch (Exception)
+            {
+                throw new Exception("The JSON content in format is not formatted correctly");
+            }
+
+            return deserializedContent;
+        }
+
+        private static string GetFileContent(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException();
+            }
+
+            return File.ReadAllText(fileName);
+        }
+    }
+}
---
+++ AngelListJobSeeker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AngelListJobSeeker.Parser;
+
+namespace AngelListJobSeeker.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = ProfileParser.ParseFile("test.json");
+
+            Console.ReadKey();
+        }
+    }
+}
